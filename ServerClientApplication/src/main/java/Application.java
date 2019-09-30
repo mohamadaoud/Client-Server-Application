9a3b73ea9e635663afda3efc6727c1d0188cea9c
Patrik.Karlsson <Patrik.Karlsson@4PBRXT2.autocom.local>
2019-09-30 21:35:35 +0200

    Modified Calculation

--- ServerClientApplication/src/main/java/Application.java
+++ ServerClientApplication/src/main/java/Application.java
@@ -13,6 +13,7 @@ import java.util.logging.*;
  *
  * @author Patrik.Karlsson
  */
+
 public class Application extends Thread {
     private Socket socket = null;
     
@@ -22,31 +23,32 @@ public class Application extends Thread {
 
     @Override
     public void run() {
-        boolean com = true;
+        boolean clientRun = true;
         PrintWriter outWriter = null;
         BufferedReader inputReader = null;
-        while (com) {
-            try {
+        try {
+            while(clientRun) {
                 outWriter = new PrintWriter(socket.getOutputStream(), true);
                 outWriter.println("Write two numbers to calculate ex. 2+2 / or quit to exit");
                 inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String inputData = inputReader.readLine();
-                if (inputData.matches("quit")) {
-                        com = false;
+                if (inputData.equalsIgnoreCase("quit")) {
+                    clientRun = false;
                 } else {
                     String result = splitData(inputData);
                     outWriter.println(inputData+" = "+result);
                 }
-            } catch (IOException e) {
-                Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, e);
             }
-        }
-        try {
-            inputReader.close();
-            outWriter.close();
-            socket.close();
-        } catch (IOException ex) {
-            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException e) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, e);
+        } finally {
+            try {
+                inputReader.close();
+                outWriter.close();
+                socket.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
     }
 
@@ -57,36 +59,24 @@ public class Application extends Thread {
             switch (Character.toString(character)) {
             case "+":
                 data = inputData.split("\\+");
-                return Integer.toString(addition(Integer.parseInt(data[0]), Integer.parseInt(data[1])));
+                return Integer.toString(Math.addExact(Integer.parseInt(data[0]), 
+                          Integer.parseInt(data[1])));
             case "-":
                 data = inputData.split("\\-");
-                return Integer.toString(subtraction(Integer.parseInt(data[0]), Integer.parseInt(data[1])));
+                return Integer.toString(Math.subtractExact(Integer.parseInt(data[0]), 
+                          Integer.parseInt(data[1])));
             case "*":
                 data = inputData.split("\\*");
-                return Integer.toString(multiplication(Integer.parseInt(data[0]), Integer.parseInt(data[1])));
+                return Integer.toString(Math.multiplyExact(Integer.parseInt(data[0]), 
+                          Integer.parseInt(data[1])));
             case "/":
                 data = inputData.split("\\/");
-                return Integer.toString(division(Integer.parseInt(data[0]), Integer.parseInt(data[1])));
+                //return Integer.toString(calcVal(Integer.parseInt(data[0]), 
+                  //        Integer.parseInt(data[1]), '/'));
             default:
                 break;
             } 
         }
         return "";
-    }
-    
-    private int addition(int val1, int val2) {
-        return val1 + val2;
-    }
-
-    private int subtraction(int val1, int val2) {
-        return val1 - val2;
-    }
-
-    private int multiplication(int val1, int val2) {
-        return val1 * val2;
-    }
-
-    private int division(int val1, int val2) {
-        return val1 / val2;
     }
 }
--- ServerClientApplication/src/main/java/Server.java
+++ ServerClientApplication/src/main/java/Server.java
@@ -14,6 +14,7 @@ import java.util.Date;
  *
  * @author Patrik.Karlsson
  */
+
 public class Server {
     
     public static void main(String[] args) throws Exception {
