f9317da53ea4fe4d88e05d98051fa350646a5ba7
Mohamad Daoud <daoud_mohamad@yahoo.co.uk>
2019-10-06 16:10:05 +0200

    Update Server.java

--- ServerClientApplication/src/main/java/Server.java
+++ ServerClientApplication/src/main/java/Server.java
@@ -5,8 +5,8 @@ import java.net.Socket;
 import java.util.Date;
 
 /*
-This is the Main class for Server. A loop that wait for serverSocket.accept() ti find a new Client.
-The Client connect on port 35000.
+This is the Main class for Server. A loop that wait for serverSocket.accept() to find a new Client.
+The Client connects on port 35000.
 */
 
 public class Server {
@@ -18,7 +18,7 @@ public class Server {
             //Listening for incoming Connections
             while (runServer) {
                 System.out.println("Connecton opened. (" + new Date() + ")");
-                //The client should accept in order to communicate with the server)
+                //The client should accept in order to communicate with the server
                 Socket socket = serverSocket.accept();
                 Application application = new Application(socket);
                 application.start();
