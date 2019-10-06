1b90df43d4990bd6f85151087af1e093186e33b4
Mohamad Daoud <daoud_mohamad@yahoo.co.uk>
2019-10-06 15:30:40 +0200

    Update Application.java

--- ServerClientApplication/src/main/java/Application.java
+++ ServerClientApplication/src/main/java/Application.java
@@ -16,6 +16,7 @@ public class Application extends Thread {
         this.socket = socket;
     }
 
+    //Reading from and Writing to a Socket
     @Override
     public void run() {
         boolean clientRun = true;
@@ -24,20 +25,26 @@ public class Application extends Thread {
         try {
             while(clientRun) {
                 outWriter = new PrintWriter(socket.getOutputStream(), true);
+                //Enter only two valid numbers with an operation in between
                 outWriter.println("Write two numbers to calculate ex. 2+2 / or quit to exit");
                 inputReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 String inputData = inputReader.readLine();
+                //Or you can simply quit the calculation by entering a "quit" word
                 if (inputData.equalsIgnoreCase("quit")) {
                     clientRun = false;
+                //Once the user has entered a value other than "quit"
                 } else {
                     String result = splitData(inputData);
                     outWriter.println(inputData+" = "+result);
                 }
             }
+            //Throws an exception once an invalid equation has been entered
+            //And connection though will be disconnected
         } catch (IOException | NumberFormatException e) {
             outWriter.println("Error: "+e.getMessage()+"\n\rConnection will be interrupted.");
         } finally {
             try {
+                //Close the socket's input stream, socket's output stream and the socket connection
                 inputReader.close();
                 outWriter.close();
                 socket.close();
@@ -47,6 +54,7 @@ public class Application extends Thread {
         }
     }
 
+    //Split the equation that has been entered (input by user), and return the result
     private String splitData(String inputData) {
         String[] data;
         for (int i = 0; i < inputData.length(); i++) {
@@ -72,6 +80,7 @@ public class Application extends Thread {
                 break;
             } 
         }
+        //Return only if the input were not numbers, and print this message to the user
         return "Unknown value. Input string may have wrong format.";
     }
 }
