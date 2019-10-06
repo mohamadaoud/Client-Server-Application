d1c2ef9a2139cceaab697bd06d3d85e120bed7c0
Mohamad Daoud <daoud_mohamad@yahoo.co.uk>
2019-10-06 15:41:17 +0200

    Update Server.java

--- ServerClientApplication/src/main/java/Server.java
+++ ServerClientApplication/src/main/java/Server.java
@@ -12,10 +12,13 @@ The Client connect on port 35000.
 public class Server {
     
     public static void main(String[] args) throws Exception {
+        //Creates a Server Socket that listens on port 35000
         try (ServerSocket serverSocket = new ServerSocket(35000)) {
             boolean runServer = true;
+            //Listening for incoming Connections
             while (runServer) {
                 System.out.println("Connecton opened. (" + new Date() + ")");
+                //The client should accept in order to communicate with the server)
                 Socket socket = serverSocket.accept();
                 Application application = new Application(socket);
                 application.start();
